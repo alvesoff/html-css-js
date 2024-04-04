5b55abc2f400d189ef53368991cca9a43f137a2b
Gustavo Alves <103228297+alvesoff@users.noreply.github.com>
2024-04-03 21:09:45 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,3 @@
 document.getElementById('botao').onclick = function() {
-    document.getElementById('mensagem'). innerHTML = 'Você clicou no botão'; 
+    document.getElementById('mensagem').innerHTML = 'Você clicou no botão'; 
 };
